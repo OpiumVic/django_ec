88046c081f47efdf53d62f451b3576949ac0cf0d
OpiumVic <mutunga.victor@strathmore.edu>
2025-11-20 20:10:40 +0300

    Add product form

--- ecommerce/products/urls.py
+++ ecommerce/products/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('',views.home, name="home"),
     path('add_category/',views.add_category, name="add_category"),
     path('category_list/',views.category_list, name="category_list"),
+    path('add_product/', views.add_product, name='add_product'),
+    path('product_list/', views.product_list, name='product_list'),
     path('product_loop/',views.product_loop, name="product_loop"),
     path('product/<int:pk>/', views.product_detail, name='product_detail'),
 ]
